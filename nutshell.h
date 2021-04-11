303a4e19cbb2961ffe9e80c5473c41791704fc5b
FosterSabatino <65423560+FosterSabatino@users.noreply.github.com>
2021-04-11 14:53:38 -0400

    Update nutshell.h

--- nutshell.h
+++ nutshell.h
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct basic_command
 {
@@ -17,3 +18,25 @@ int indexCommands;
 void insert_arg(struct basic_command* Command, char* arg);
 void set_space(struct basic_command* Command, int size);
 void execute_other_commands();
+
+int setEnv(char *var, char *word);
+int printEnv();
+int unsetEnv(char *var);
+char* replaceString(char* word, char *old, char *new1);
+int chgDir(char *dir);
+int printAlias();
+int rmAlias(char *word);
+int wildcard(char *word);
+int newcmp(const void *str1, const void *str2);
+char* delChar(char *word, char c);
+
+struct aTable {
+	char name[128][100];
+	char word[128][100];
+};
+
+struct aTable aliasTable;
+
+int aliasIndex;
+bool unalias1;
+bool alias1;
